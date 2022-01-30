7c93689b5dfff9f587fdcdf8c215f9d01efb4dff
AOrps <aeo34@njit.edu>
2022-01-30 14:58:13 -0500

    Updated README + Listen() func + prelim curl func

---
+++ middleware/serve.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Development Server
+php -S localhost:9998
+
+# Real Server
+# Just have the php in the spot
--- middleware/server.php
+++ middleware/server.php
@@ -1,5 +1,72 @@
 <?php
+// Get PHP to report all errors
+error_reporting(E_ALL);
+
+// Some text to output to the page
+echo "Middleware Section"."<br>";
+
+// Arr Instantiate
+$arr = array();
+
+function cURL_POST(string $url) {
+    // Initalize 
+    $ch = curl_init();
+
+    // Curl Setopt info
+    curl_setopt($ch, CURLOPT_URL, $url); 
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, 
+        http_build_query(
+            array(
+                "username" => $_POST["username"],
+                "password" => $_POST["password"],
+            )
+        )
+    );
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 
+    // 
+    $response = curl_exec($ch);
+
+    curl_close($ch);
+}
+
+
+
+
+
+// Listen section
+function Listen($array) : Array {
+    $content = file_get_contents("php://input");
+    // echo $content."<br>";
+    $pairs = explode("&", $content);
+    // echo gettype($content)."<br>";
+    // echo count($pairs)."<br>";
+
+    for($i = 0; $i < count($pairs); $i++) {
+        // Split pairs by key and value
+        $key_value = explode("=",$pairs[$i]);
+
+        // Creating a variable for key and values
+        $key = $key_value[0];
+        $val = $key_value[1];
+
+        // Append key and values to return array
+        $array[] = $key;
+        $array[] = $val; 
+        // echo "$key\t$val"."<br>";
+    }
+    return $array;
+}
+
+
+
+
+$arr = Listen($arr);
+// Listen($arr);
+// Printing Output of $arr
+echo json_encode($arr);
 
 ?>
