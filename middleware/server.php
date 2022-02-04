5611bc2451ebc8e4fe3c47517678bbba3df6f2fb
AOrps <aeo34@njit.edu>
2022-02-03 21:18:56 -0500

    Removed buggy middleware code deleted serve.sh and deleted READMEs + unecessary code

--- middleware/server.php
+++ middleware/server.php
@@ -1,19 +1,6 @@
 <?php
-
-/*
-TODO: 
-- Change Server to Backend
-*/
-
-
 // Get PHP to report all errors
 error_reporting(E_ALL);
-
-// Some text to output to the page
-echo "Middleware Section"."<br>";
-
-// Arr Instantiate (will later become dictionary/hashmap)
-$arr = array();
 
 // making curl more modular
 function cURL_POST(string $url, array $arr) : string {
@@ -30,47 +17,18 @@ function cURL_POST(string $url, array $arr) : string {
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     // output response from curl
-    $response = curl_exec($ch);
+    $output = curl_exec($ch);
 
     // close curl resource to free system resources
     curl_close($ch);
 
     // return response from curl : string
-    return $response;
+    return $output;
 }
-
-
-// Listen input in Server Body
-function Listen($array) : Array {
-    // Listen for POST request info
-    $content = file_get_contents("php://input");
-
-    // Split data into key-value pairs
-    $pairs = explode("&", $content);
-
-    for($i = 0; $i < count($pairs); $i++) {
-        // Split pairs by key and value
-        $key_value = explode("=",$pairs[$i]);
-
-        // Creating a variable for key and values
-        $key = $key_value[0];
-        $val = $key_value[1];
-
-        // Append key and values to return "array" (dictionary)
-        $array[$key] = $val;
-        // $array[] = $val; 
-    }
-    return $array;
-}
-
-
-// set $arr so that odd indexes == keys && even indexes == values 
-$arr = Listen($arr);
 
 // sends $arr from php://input to curl backend
-$res = cURL_POST("https://web.njit.edu/~aeo34/server.php", $arr);
+$response = cURL_POST("https://web.njit.edu/~aeo34/server.php", $arr);
 
 // Printing Output of $arr
-echo json_encode($arr)."<br>";
-
+echo json_encode($_POST);
 ?>
