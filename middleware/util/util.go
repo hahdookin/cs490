3757b5f1a3fe25db002c09f8972ba2571237d184
AOrps <aeo34@njit.edu>
2022-03-25 19:15:58 -0400

    cors v2 trial

--- middleware/util/util.go
+++ middleware/util/util.go
@@ -28,7 +28,7 @@ func GetStringInBetween(str, start, end string) string {
 }
 
 func EnableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	(*w).Header().Set("Access-Control-Allow-Methods", "GET,POST")
+	(*w).Header().Set(`Access-Control-Allow-Origin`, "*")
+	(*w).Header().Set(`Access-Control-Allow-Headers`, "Content-Type")
+	(*w).Header().Set(`Access-Control-Allow-Methods`, "GET,POST")
 }
