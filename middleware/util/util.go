0c3a6e4e6f5b0ce1cf932180ca2005ca0b170f6c
AOrps <aeo34@njit.edu>
2022-03-25 19:12:32 -0400

    cors plz plz why do you send me your toughest battles

--- middleware/util/util.go
+++ middleware/util/util.go
@@ -29,4 +29,6 @@ func GetStringInBetween(str, start, end string) string {
 
 func EnableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	(*w).Header().Set("Access-Control-Allow-Methods", "GET,POST")
 }
